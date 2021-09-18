139fba41a23ec63e5d88b77c9311db31ad0e6d94
DanYTz <danialsaifullizan@gmail.com>
2021-09-18 16:14:25 +0800

    added comment for documentation and fix exit bug

--- Youtube Downloader.py
+++ Youtube Downloader.py
@@ -1,39 +1,47 @@
 from pytube import YouTube
 
+url = input('Enter Youtube Url: ')  # variable to receive the Youtube Url
+video = YouTube(url)  # this variable to convert the url to video
 
-url = input('Enter Youtube Url: ') #variable to receive the Youtube Url
-video = YouTube(url)
+print('Video Title: ' + video.title)  # to show the video title
+confirm = input('Is the url correct? (y/n):')  # to make sure the url is correct based on video's title
 
-print('Video Title: ' + video.title)
-confirm = input('Is the url correct? (y/n):')
-
-
+# if user say no
 while confirm != 'y':
-    url = input('Enter Youtube Url: ')
+    url = input('Enter Youtube Url: ')  # ask the user reenter the url
 
-    print('Video Title: ' + video.title)
-    confirm = input('Is the url correct? (y/n):')
+    print('Video Title: ' + video.title)  # to show the video title
+    confirm = input('Is the url correct? (y/n):')  # to make sure the url is correct based on video's title
 
-print('Choose the video resolution:')
+# to choose the video resolution
+print('Choose the video resolution:')  #
 print('Please wait loading.............')
-for i in video.streams: print(str(i.resolution))
-res = input("e.g.(1080p): ")
-stream = video.streams.filter(res=res).first()
-print()
-print('Title: '+ video.title)
-print('Resolution: ' + res)
-print("File Size: " + str(round(stream.filesize / 1000000, 2)) + 'MB')
+for i in video.streams:  
+    print(str(i.resolution))  # to show the list of available resolution
+res = input("e.g.(1080p): ")  # ask the user to enter the resolution based on the given resolution in the previous line
+stream = video.streams.filter(
+    res=res).first()  # change the video  resolution based on what the user type in the previous line
 
-confirm2 = input('Continue?(y/n): ')
+# to show all the setting and information about the video
+print()
+print('Title: ' + video.title)  # video title
+print('Resolution: ' + res)  # video resolution
+print("File Size: " + str(
+    round(stream.filesize / 1000000, 2)) + 'MB')  # file size (need to round it into 2 decimal number)
+
+# to confirm before proceed
+confirm2 = input('Continue?(y/n): ')  # ask the user to continue
+
+# if the user say no
 while confirm2 != 'y':
-    exit = input('Do you want to exit?(y/n): ')
-    if exit == 'y':
-        quit()
+    stop_all = input('Do you want to exit?(y/n): ')  # ask if the user want to exit the program
+    if stop_all == 'y':
+        exit()
     else:
-        confirm2 = input('Continue?(y/n): ')
+        confirm2 = input('Continue?(y/n): ')  # if the user don't want to exit, ask the user to proceed
 print()
 print('Downloading......')
 
-stream.download()
+stream.download()  # start downloading
 print()
 print('Download Completed')
