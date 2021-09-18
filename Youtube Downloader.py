3927fd56ef38501fa488254e71a7aaf3c0a82e57
DanYTz <danialsaifullizan@gmail.com>
2021-09-18 12:37:37 +0800

    Added confirm verification and download function

--- Youtube Downloader.py
+++ Youtube Downloader.py
@@ -4,3 +4,21 @@ url = input('Enter Youtube Url: ')
 video = YouTube(url)
 
 print('Video Title: ' + video.title)
+confirm = input('is the url correct? (y/n):')
+
+while confirm != 'y':
+    url = input('Enter Youtube Url: ')
+
+    print('Video Title: ' + video.title)
+    confirm = input('is the url correct? (y/n):')
+
+confirm2 = input('continue?(y/n): ')
+while confirm2 != 'y':
+    exit = input('Do you want to exit?(y/n): ')
+    if exit == 'y':
+        exit()
+    else:
+        confirm = input('is the url correct? (y/n):')
+print('downloading......')
+stream = video.streams.get_lowest_resolution()
+stream.download()
